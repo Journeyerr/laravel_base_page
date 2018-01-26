d78c252a9ad386dc3c191f1b1d721fa9c83b79e2
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-01-26 11:33:32 +0800

    add herad and footer views

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img  src="https://avatars3.githubusercontent.com/u/24761948?s=40&v=4" alt="" style="width: 17px;height: 17px;">
+            <a href="http://www.echophp.top">
+                山前没相见  山后别相逢
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="/about" target="_blank">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,16 @@
 @section('title','主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>ZAHANG AN YUAN </h1>
+        <p class="lead">
+            你现在所看到的是 <a href="http://www.echophp.top"> 我 </a>  Laravel 的学习之路。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @endsection
