b43cb311a59b2b6915bfa0378723ec156b007c94
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-01-26 12:14:32 +0800

    move User.php to models folder

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,14 +2,14 @@
     <footer class="footer">
         <small class="slogon">
             <img  src="https://avatars3.githubusercontent.com/u/24761948?s=40&v=4" alt="" style="width: 17px;height: 17px;">
-            <a href="http://www.echophp.top">
-                山前没相见  山后别相逢
-            </a>
-        </small>
-        <nav>
-            <ul>
-                <li><a href="/about" target="_blank">关于</a></li>
-            </ul>
-        </nav>
-    </footer>
+<a href="http://www.echophp.top">
+    山前没相见  山后别相逢
+</a>
+</small>
+<nav>
+    <ul>
+        <li><a href="/about" target="_blank">关于</a></li>
+    </ul>
+</nav>
+</footer>
 </div>
