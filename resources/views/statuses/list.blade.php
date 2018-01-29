740cd5adbee369b56314c9e34cb2a3d84859f46d
zhanganyuan <zhanganyuan@qiyiniu.cn>
2018-01-29 17:30:40 +0800

    edit status list style

--- resources/views/statuses/list.blade.php
+++ resources/views/statuses/list.blade.php
@@ -1,4 +1,12 @@
 <li id="status-{{ $status->id }}">
+
+    <a href="{{ route('users.show', $user->id )}}">
+        <img src="/image/longmao.png" alt="{{ $user->name }}"  class="gravatar " style="display: inline-block;height: 35px;width: 35px;"/>
+    </a>
+
+    <span class="user">
+        <a href="{{ route('users.show', $user->id )}}">{{ $user->name }}</a>
+    </span>
     {{--删除按钮--}}
     @can('destroy', $status)
         <form action="{{ route('statuses.destroy', $status->id) }}" method="POST">
@@ -7,13 +15,6 @@
             <button type="submit" class="btn btn-sm btn-danger status-delete-btn" style="float: right">删除</button>
         </form>
     @endcan
-    <a href="{{ route('users.show', $user->id )}}">
-        <img src="/image/longmao.png" alt="{{ $user->name }}"  class="gravatar " style="display: inline-block;height: 35px;width: 35px;"/>
-    </a>
-
-    <span class="user">
-        <a href="{{ route('users.show', $user->id )}}">{{ $user->name }}</a>
-    </span>
     <span class="timestamp" style="display: block;margin-left: 60px;color:#999;">
         {{ $status->created_at->diffForHumans() }}
     </span>
